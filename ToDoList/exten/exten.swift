13172784d68f7c00cdfd8e8b8bf7d0b93afea871
joe <xiaozhishang1108@outlook.com>
2022-09-17 14:55:29 +0800

    time

--- ToDoList/exten/exten.swift
+++ ToDoList/exten/exten.swift
@@ -1,169 +1,100 @@
+//
+//  exter.swift
+//  exter
+//
+//  Created by 陆骁剑 on 2022/9/17.
+//  Copyright © 2022 Moon. All rights reserved.
+//
+
 import WidgetKit
 import SwiftUI
+import Intents
 
-struct Provider: TimelineProvider {
+struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        return WeatherPoser.dafaultEntry()
+        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
     }
-    
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = WeatherPoser.dafaultEntry()
+
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), configuration: configuration)
         completion(entry)
     }
-    
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+
+    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
-        //设定1小时更新一次数据
-        let updateDate = Calendar.current.date(byAdding: .minute, value: 1, to: currentDate)!
-        
-        let entry = WeatherPoser.dafaultEntry()
-        let timeline = Timeline(entries: [entry], policy: .after(updateDate))
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .second, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
-        
-    
-         /*// 真实网络请求
-         WeatherPoser.requestNow { result in
-         if case .success(let now) = result {
-         let entry = Entry(date: currentDate, dataList: poster, cityName: String(name), today: now)
-         let timeline = Timeline(entries: [entry], policy: .after(updateDate))
-         completion(timeline)
-         }
-         }*/
-         
-        
     }
-    
 }
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
-    let dataList: [WeatherModel]
-    let cityName: String
-    let today: Now
+    let configuration: ConfigurationIntent
 }
-
-struct WeatherWidgetEntryView : View {
-    var entry: Provider.Entry
-    @Environment(\.widgetFamily) var family:WidgetFamily
+//获取当天开始的日期，给Date增加一个拓展方法
+ extension Date {
+    func getCurrentDayStart(_ isDayOf24Hours: Bool)-> Date {
+        let calendar:Calendar = Calendar.current;
+        let year = calendar.component(.year, from: self);
+        let month = calendar.component(.month, from: self);
+        let day = calendar.component(.day, from: self);
     
-    var body: some View {
-        switch family {
-        case .systemSmall:
-            VStack {
-                Text("body")
-            }
-        case .systemMedium:
-            MediumWeatherView(entry: entry)
-        case .systemLarge:
-            VStack {
-                Text("body")
-            }
-        default:
-            MediumWeatherView(entry: entry)
-        }
+        let components = DateComponents(year: year, month: month, day: day, hour: 0, minute: 0, second: 0)
+        return Calendar.current.date(from: components)!
     }
+}
+// 实现一天内的计时器
+//Text(Date().getCurrentDayStart(true), style: .timer)
+
+struct exterEntryView : View {
+    // 这句代码能从上下文环境中取到小组件的型号
+        @Environment(\.widgetFamily) var family
+        
+        // 组件数据
+        var entry: Provider.Entry
+
+        // 这个 body 中就是自己需要实现的组件布局
+        var body: some View {
+            switch family {
+            case .systemSmall:  // 小号
+                Text(entry.date, style: .time)
+            case .systemMedium: // 中号
+                Text(Date().getCurrentDayStart(true), style: .timer)
+                    .font(.system(size: 75))
+                    .bold()
+            case .systemLarge:  // 大号
+                Text(entry.date, style: .time)
+            @unknown default:
+                Text(entry.date, style: .time)
+            }
+        }
 }
 
 @main
-struct WeatherWidget: Widget {
-    let kind: String = "WeatherWidget"
-    
+struct exter: Widget {
+    let kind: String = "exter"
+
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            WeatherWidgetEntryView(entry: entry)
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+            exterEntryView(entry: entry)
         }
-        .configurationDisplayName("天气")
-        .description("显示最近天气预报")
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
     }
 }
 
-struct WeatherWidget_Previews: PreviewProvider {
+struct exter_Previews: PreviewProvider {
     static var previews: some View {
-        let entry = WeatherPoser.dafaultEntry()
-        WeatherWidgetEntryView(entry: entry)
+        exterEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
-}
-
-struct MediumWeatherView: View {
-    var entry: Provider.Entry
-    var body: some View {
-        VStack {
-            HStack(alignment: .center){
-                Image("blue_loc").frame(width: 12, height: 12, alignment: .center).aspectRatio(contentMode: .fit)
-                Text(self.entry.cityName)
-                Spacer()
-                Text(self.entry.date, style: .date)
-            }
-            HStack(alignment: .center){
-                if let text = self.entry.today.text {
-                    Image(text).frame(width: 44, height: 44, alignment: .center).aspectRatio(contentMode: .fit)
-                    let temperature = self.entry.today.temperature
-                    Text((temperature + "°"))
-                        .font(.largeTitle)
-                        .fontWeight(.semibold)
-                }
-                ForEach(entry.dataList) {item in
-                    if item.id == 0 {
-                        VStack {
-                            Text(item.weather)
-                            Text(item.temp)
-                        }
-                    }
-                }
-                Spacer()
-            }
-            Spacer()
-            HStack(alignment: .center){
-                ForEach(entry.dataList) {item in
-                    if item.id > 0 {
-                        WeatherDayView(item:item)
-                    }
-                }
-            }
-        }.padding(11.0)
-    }
-}
-
-struct WeatherDayView: View {
-    var item: WeatherModel
-    var body: some View {
-        HStack(alignment: .center, spacing: 12){
-            Image(self.item.weather).frame(width: 30, height: 30, alignment: .center).aspectRatio(contentMode: .fit)
-            VStack(alignment: .leading) {
-                Text(self.item.title)
-                    .font(.body)
-                    .foregroundColor(Color(hue: 0.275, saturation: 0.74, brightness: 0.621))
-                Text(self.item.temp)
-                    .font(.body)
-            }
-        }
-    }
-}
-
-
-struct Now: Identifiable {
-    var temperature: String = ""
-    var text: String = ""
-    var id: Int = 0
-}
-struct WeatherModel: Identifiable {
-    var weather: String = ""
-    var title: String = ""
-    var temp: String = ""
-    var id: Int = 0
-}
-
-struct WeatherPoser {
-    static func dafaultEntry() -> SimpleEntry {
-        let dataList = [
-            WeatherModel(weather: "晴", title: "今天", temp: "20/36°", id: 0),
-            WeatherModel(weather: "晴", title: "明天", temp: "20/36°", id: 1),
-            WeatherModel(weather: "晴", title: "后天", temp: "20/36°", id: 2),
-            WeatherModel(weather: "晴", title: "周四", temp: "20/36°", id: 3)
-        ]
-        let now = Now(temperature: "30", text: "多云", id: 0)
-        return SimpleEntry(date: Date(), dataList: dataList, cityName: "北京市", today: now)
-    }
-    
 }
